2eb34a8a575ab5d313d6ecb163121b5e62101f67
Daniel Calle <dcalle@xmltravelgate.com>
2017-08-19 21:56:54 +0200

    En este comit al correr el archvo con node index.js tendremos un endpoint y un graphiql

--- server-graphql/index.js
+++ server-graphql/index.js
@@ -1,7 +1,22 @@
 const express = require ('express')
 const bodyParser = require ('body-parser')
+const { graphqlExpress, graphiqlExpress } = require ('apollo-server-express')
+const schema = require ('./schema')
 
 const app = express()
+
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema:schema })
+)
+
+app.use(
+  '/graphiql'
+  graphiqlExpress({
+    endpointURL: '/graphql'
+  })
+)
 
 const PORT = 5678
 app.listen(PORT, () =>{
